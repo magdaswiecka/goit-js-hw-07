ccd4a0ff6ed5e5f74530f9af8591e2c448fc8da2
magdaswiecka <magstaron@gmail.com>
2022-10-23 18:08:20 +0200

    Zadanie 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+
+const myGallery = document.querySelector(".gallery");
+
+for(const galleryItem of galleryItems) {
+  const myDiv = document.createElement("div");
+  const myA = document.createElement("a")
+  const myImg = document.createElement("img")
+
+  myDiv.className = "gallery__item";
+  myA.className = "gallery__link"
+  myA.href = galleryItem.original
+  myImg.className = "gallery__image"
+  myImg.src = galleryItem.preview
+  myImg.dataset.source = galleryItem.original
+  myImg.alt = galleryItem.description
+
+  myGallery.append(myDiv)
+  myDiv.append(myA)
+  myA.append(myImg)
+}
+
+myGallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  
+  const clickedImg = event.target;
+
+  const instance = basicLightbox.create(`
+    <img src="${clickedImg.dataset.source}">
+  `);
+
+  instance.show();
+});
